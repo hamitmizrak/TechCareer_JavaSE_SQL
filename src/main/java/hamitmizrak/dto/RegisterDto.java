976bb66ce64f1191af4cdfa629cd6ec3ff6372e9
hamitmizrak <hamitmizrak@gmail.com>
2023-06-02 15:17:42 +0300

    RegisterDto

--- src/main/java/hamitmizrak/dto/RegisterDto.java
+++ src/main/java/hamitmizrak/dto/RegisterDto.java
@@ -1,4 +1,21 @@
 package hamitmizrak.dto;
 
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+
+import java.util.Date;
+// LOMBOK
+@Data // @Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
 public class RegisterDto {
-}
+    //Field
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private Date createdDate;
+} //end class
