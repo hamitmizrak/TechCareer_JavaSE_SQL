298aeb768506409712ef1c74bc5925da7eb013c5
hamitmizrak <hamitmizrak@gmail.com>
2023-06-02 14:07:50 +0300

    Initial commit

---
+++ src/main/java/hamitmizrak/TechCareerJavaSeSqlApplication.java
@@ -0,0 +1,13 @@
+package hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareerJavaSeSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCareerJavaSeSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hamitmizrak/TechCareerJavaSeSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package hamitmizrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerJavaSeSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
