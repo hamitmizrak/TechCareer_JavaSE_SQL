169fe8395d0682daea134d1a632e9ba2e0774dd8
hamitmizrak <hamitmizrak@gmail.com>
2023-06-02 14:44:14 +0300

    Sql persist Table

--- src/main/java/hamitmizrak/sql/Persist_All_Sql.java
+++ src/main/java/hamitmizrak/sql/Persist_All_Sql.java
@@ -14,5 +14,16 @@ public class Persist_All_Sql {
      -- Eğer birden fazla DB varsa sadece ilgili DB set
      use blog;
 
+     -- Create Table
+     CREATE TABLE `blog`.`register` (
+     `id` INT NOT NULL AUTO_INCREMENT,
+     `name` VARCHAR(255) NULL DEFAULT 'Adını girmediniz',
+     `surname` VARCHAR(255) NULL DEFAULT 'Soyadını girmediniz',
+     `email` VARCHAR(255) NULL DEFAULT 'Email girmediniz',
+     `password` VARCHAR(255) NULL DEFAULT 'Şifre girmediniz',
+     `created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (`id`))
+     ENGINE = InnoDB;
+
      */
 }
